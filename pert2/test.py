9b7e34d33b666645764c1ddebfada7f176813f75
root <root@DESKTOP-3H8LB9E>
2024-07-20 00:38:18 +0700

    first commit

---
+++ pert2/test.py
@@ -0,0 +1,21 @@
+# Mendefinisikan variabel dengan nilai string
+nama = "Raihan Fadhli Ramadhan"
+umur = 20
+
+# Menampilkan nilai variabel
+print("Nama:", nama)
+print("Umur:", umur)
+
+# Mengubah nilai variabel
+umur = 35
+
+# Menampilkan nilai variabel yang telah diubah
+print("Umur sekarang:", umur)
+
+# Menggunakan variabel dalam ekspresi matematika
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+
+# Menampilkan hasil dari ekspresi matematika yang menggunakan variabel
+print("Luas persegi panjang:", luas)
